3a7670212803bea526159212168af29a03a6eb50
Aldea Cosmin <aldeacosmin@petrapavaje.ro>
2026-05-20 19:29:34 +0300

    fix: update Google Maps embed to Astoria Hotel coordinates (46.122656, 23.622188)

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -146,7 +146,7 @@ export function ContactPage() {
               {/* Map */}
               <div className="aspect-video rounded-sm overflow-hidden border border-border">
                 <iframe
-                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2775.5!2d23.5833!3d46.0667!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDbCsDA0JzAwLjAiTiAyM8KwMzUnMDAuMCJF!5e0!3m2!1sen!2sro!4v1"
+                  src="https://maps.google.com/maps?q=46.122656,23.622188&z=15&output=embed&hl=ro"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
